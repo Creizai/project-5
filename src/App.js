288ad17584b0146c47a83833e779f99a7ad0c837
Adam_Bates <Creizai@EMAIL.COM>
2019-12-14 18:55:40 -0500

    loaded in reset.css, also set up a BASIC w3 schools Template, currently broke due to style and other small things. Need to play more before creating components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
+import './reset.css'
 import './App.css';
 
+const City = [
+  "London",
+  "Paris",
+  "Tokyo"
+]
+
 function App() {
+
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Tabs Wireframe</h1>
       </header>
+      <main className="WireFrame-Box">
+        <div className="Button-Box">
+          <button className="button-tab" onclick="openCity('')">London</button>
+          <button className="button-tab" onclick="openCity('Paris')">Paris</button>
+          <button className="button-tab" onclick="openCity('Tokyo')">Tokyo</button>
+        </div>
+
+        <div id="London" className="w3-container city">
+          <h2>London</h2>
+          <p>London is the capital city of England.</p>
+        </div>
+
+        <div id="Paris" className="w3-container city" style="display:none">
+          <h2>Paris</h2>
+          <p>Paris is the capital of France.</p>
+        </div>
+
+        <div id="Tokyo" className="w3-container city" style="display:none">
+          <h2>Tokyo</h2>
+          <p>Tokyo is the capital of Japan.</p>
+        </div>
+      </main>
     </div>
   );
 }
